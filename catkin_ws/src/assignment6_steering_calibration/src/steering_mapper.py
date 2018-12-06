7b52a14fb9ac65899108a5f2c64e087990949e17
Steve Dierker <steve.dierker@obstkiste.org>
2018-12-06 19:26:44 +0100

    final pdf.

---
+++ catkin_ws/src/assignment6_steering_calibration/src/steering_mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class SteeringMapper:
+    def __init__(self, angles):
+        self.angles = angles
+
+    def mapp(self, steering_angle):
+
+
+if __name__ == '__main__':
+    angles = [(0,), (30, ), (60, ), (90, ), (120, ), (150, ), (180, )]
+    sm = SteeringMapper(angles)
