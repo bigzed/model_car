726b9c6d2270006f46300db4c6b0dada1b4fa235
Semjon Kerner <semjon.kerner@fu-berlin.de>
2019-01-30 13:21:30 +0100

    merge distant_point.py into car program

--- catkin_ws/src/assignment10_look_ahead_point_and_gps/src/line_detection.py
+++ catkin_ws/src/assignment10_look_ahead_point_and_gps/src/line_detection.py
@@ -10,7 +10,7 @@ import time
 import tf
 
 from sklearn import linear_model
-from scipy.misc import imread
+from imageio import imread
 from cv_bridge import CvBridge, CvBridgeError
 from collections import namedtuple
 from sensor_msgs.msg import Image
@@ -256,56 +256,96 @@ class Localization:
         distpoint = self.closest_point([x,y], 1, 20)
         v = distpoint - [x,y]
         errorangle = np.arccos((v[0]*u[0] + v[1]*u[1]) / (np.linalg.norm(v) * np.linalg.norm(u)))
-        print(distpoint, [x,y])
-        #print(distpoint)
 
-        #self.error_pub.publish(Int16(distance))
+        self.error_pub.publish(Int16(errorangle * 2))
 
     def closest_point(self, point, laneID, distance):
-        x,y = point[0], point[1]
-
+        """Returns closest point on trajectory."""
+        # Top or bottom center of circle
+        x, y = point[0], point[1]
         if laneID == 1: #INNERLANE
             laned = 16
         elif laneID == 2: #OUTERLANE
             laned = 48
 
-        """Returns closest point on trajectory."""
-        # Top or bottom center of circle
         if x == 215 and y == 196:
             return np.array([215, 76 + laned])
         if x == 215 and y == 404:
             return np.array([215, 524 + laned])
 
+        dp = [0,0]
+        cp = [point]
         # Top half circle:
-        if y <= 196:
-            return self.dist_on_circle(np.array([x, y]), np.array([215, 196]), 121 + laned, distance)
-        # Bottom half circle:
-        elif y >= 404:
-            return self.dist_on_circle(np.array([x, y]), np.array([215, 404]), 121 + laned, distance)
-        # Left line
-        elif x <= 215:
-            if y + distance >= 404:
-                return self.closest_point_on_circle(np.array([x, y - distance]), np.array([215, 196]), 121 + laned)
-            return np.array([94 - laned, y + distance])
-        # Right line
-        else:
-            if y - distance >= 196:
-                return self.closest_point_on_circle(np.array([x, y - distance]), np.array([215, 404]), 121 + laned)
-            return np.array([336 + laned, y - distance])
+        distance += 1
+        first = True
+        while (distance > 0 or first):
+            first = False
+            if y <= 196:
+                dp,distance,ncp = closest_point_on_circle(np.array([x, y]),
+                        np.array([215, 196]), 121 + laned, distance)
+                if distance > 0:
+                    dp[0],dp[1] = 94 - laned, 197
+            # Bottom half circle:
+            elif y >= 404:
+                dp,distance,ncp = closest_point_on_circle(np.array([x, y]), np.array([215, 404]),
+                        121 + laned, distance)
+                if distance > 0:
+                    dp[0],dp[1] = 336 + laned, 403
+            # Left line
+            elif x <= 215:
+                ncp = np.array([94 - laned, y])
+                if y + distance > 404:
+                    distance -= (404 - y)
+                    dp = np.array([94 - laned, 404])
+                else:
+                    dp = np.array([94 - laned, y + distance])
+                    distance = 0
+            # Right line
+            else:
+                ncp = np.array([336 + laned, y])
+                if y - distance < 196:
+                    distance -= (y - 196)
+                    dp = np.array([336 + laned, 196])
+                else:
+                    dp = np.array([336 + laned, y - distance])
+                    distance = 0
+            x,y = dp[0],dp[1]
+            cp.append(ncp)
+        cp.append(dp)
+        return cp
 
-    def closest_point_on_circle(self, p, c, r):
+    def closest_point_on_circle(self, p, c, r, d):
         v = p - c
-        return c + v / np.linalg.norm(v) * r
+        closepoint = c + ((v * r) / np.linalg.norm(v))
 
-    def dist_on_circle(self, p, c, r, dist):
-        v = p - c
-        distpoint = c + (v * r / np.linalg.norm(v))
-        U = 2 * np.pi * r
-        distanceangle = 360 * (dist / U)
-        c, s = np.cos(distanceangle), np.sin(distanceangle)
-        distpoint[0] = distpoint[0]*c + distpoint[1]*(-s)
-        distpoint[1] = distpoint[0]*s + distpoint[1]*(c)
-        return distpoint
+        if d > 0:
+            top = False
+            if c[1] == 196:
+                top = True
+            dist = 0
+            turnpoint = closepoint
+            U = (2 * np.pi * r)
+            if d > (U/2):
+                temp = d % (U/2)
+                dist = d - temp
+                d = temp
+            distangle = (360 * (d / U))
+            if top:
+                ca = np.array([1,0])
+                pangle = np.degrees(np.arccos(np.dot(v,ca) / (np.linalg.norm(v) * np.linalg.norm(ca))))
+                tangle = 360-(pangle + distangle) # reverse target angle
+                if tangle < 180:
+                    dist += (180 - tangle) * (U/360)
+            else: #bot
+                ca = np.array([-1,0])
+                pangle = np.degrees(np.arccos(np.dot(v,ca) / (np.linalg.norm(v) * np.linalg.norm(ca))))
+                tangle = 180-(pangle + distangle) # reverse target angle
+                if tangle < 0:
+                    dist += abs(tangle) * (U/360)
+            cos = r * np.cos(np.deg2rad(tangle))
+            sin = r * np.sin(np.deg2rad(tangle))
+            turnpoint = np.array([cos, sin]) + c
+        return turnpoint, dist, closepoint
 
     def plot(self):
         # Error
@@ -322,8 +362,6 @@ class Localization:
         x, y = np_positions.T
         plt.plot(x, y, 'g')
         plt.show(block=True)
-
-
 
 def main(args):
     rospy.init_node("oval_circuit")
